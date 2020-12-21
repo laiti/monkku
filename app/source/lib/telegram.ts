b09e7e3b78fa41080ea2198644b3872d6d4d53fa
Antti Laiti <antti@laiti.fi>
2020-12-21 19:38:59 +0200

    Prettier

--- app/source/lib/telegram.ts
+++ app/source/lib/telegram.ts
@@ -1,6 +1,6 @@
 export default class Telegram {
-
-    async SendMsg (message: string): Promise<boolean> {
-        return true;
-    }
+  async SendMsg(message: string): Promise<boolean> {
+    console.log(message);
+    return true;
+  }
 }
