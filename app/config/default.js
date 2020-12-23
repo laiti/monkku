3d11342c40a27d35a0b7b9bd63a05a52902a3f1d
Antti Laiti <antti@laiti.fi>
2020-12-23 17:29:50 +0200

    Add default config

---
+++ app/config/default.js
@@ -0,0 +1,34 @@
+const configuration = {
+  telegram: {
+    chatId: '',
+  },
+  aws: {
+    region: 'eu-west-1',
+  },
+  database: {
+    dynamoDB: {
+      tableName: '',
+      apiVersion: '2012-08-10',
+    },
+    mongo: {
+      user: '',
+      password: '',
+      database: '',
+    },
+  },
+
+  // If dynamic  is set, it overrides the static times
+  times: {
+    static: ['13:34', '13:37', '14:27'],
+    dynamic: false,
+  },
+
+  // Give bonus point for each bonusPerSec seconds that occur
+  // between the /monkku command and end of last time in case
+  // there were no other players
+  score: {
+    bonusPerSec: 10,
+  },
+};
+
+module.exports = configuration;
