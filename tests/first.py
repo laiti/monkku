30f503b54b14a5b5ed790bab240832d3e3804102
Antti Laiti <antti@laiti.fi>
2021-01-01 10:46:13 +0200

    Add testing message

--- tests/first.py
+++ tests/first.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-
 import json
+import telegram
 
 with open("config.json") as json_config_file:
     config = json.load(json_config_file)
+
+player1 = telegram.Bot(token=config["players"][1])
+player1.sendMessage(chat_id=config["chat_id"]["correct"], text="testing")
