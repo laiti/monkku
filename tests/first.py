a7678da0b432b66d77e9f462a2eee23e8fb6f10b
Antti Laiti <antti@laiti.fi>
2021-01-02 15:11:52 +0200

    Pylint and gitignore for vscode files

--- tests/first.py
+++ tests/first.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Script for pushing the initial participation for Monkku
+"""
+
 import json
 import telegram
 
@@ -8,7 +12,7 @@ with open("config.json") as json_config_file:
 # Set up all players
 players = {}
 for player in config["players"]:
-  players[player] = telegram.Bot(token=config["players"][player])
+    players[player] = telegram.Bot(token=config["players"][player])
 
 for player in players:
     players[player].sendMessage(chat_id=config["chat_id"]["correct"], text="/monkku")
