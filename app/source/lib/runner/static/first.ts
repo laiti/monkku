e8f0682d898703a62f80132a9a98a154b77b59a0
Antti Laiti <antti@laiti.fi>
2021-12-06 20:48:34 +0200

    Detect empty response (#4)
    * Detect empty response and retry
    
    * Add generator for retry message
    
    * Fix linter errors
    
    * Fix pylint warnings
    
    * Update attempt amount
    
    * remove TODO
    
    * Use retry message in config
    
    * Do not put seconds to message

--- app/source/lib/runner/static/first.ts
+++ app/source/lib/runner/static/first.ts
@@ -1,6 +1,6 @@
 import { TelegramClient } from 'messaging-api-telegram';
 import TelegramParser from '../../telegram/parser';
-// import { Update } from 'messaging-api-telegram/dist/TelegramTypes';
+import { Update } from 'messaging-api-telegram/dist/TelegramTypes';
 import TimeGenerator from '../../util/time';
 import MessageWriter from '../../telegram/writer';
 import { Config } from '../../../types/config';
@@ -24,15 +24,39 @@ export default class FirstMonkku {
     this.parser = new TelegramParser(this.log);
     this.client = new TelegramClient({ accessToken: config.telegram.apiKey });
   }
+  private getUpdates(): Promise<Update[]> {
+    return this.client.getUpdates({
+      allowedUpdates: ['message'],
+      limit: 100,
+      timeout: this.config.telegram.timeout,
+    });
+  }
+
   async run(): Promise<boolean> {
     this.log.info('Running first monkku');
     // Get last messages from Telegram
-    const updates = this.client.getUpdates({
-      allowedUpdates: ['message'],
-      limit: 100,
-      timeout: 10,
-    });
-    // TODO: check for empty updates from API
+    let updates = [];
+    let attempts = 0;
+    let waitTime = 0;
+    while (
+      updates === undefined ||
+      updates.length == 0 ||
+      attempts >= this.config.telegram.maxAttempts
+    ) {
+      if (attempts > 1) {
+        // If we've tried already once, we inform about it and wait for some time before the retry
+        waitTime = waitTime + attempts * 10000;
+        const retryMessage = this.message.retry();
+        this.client.sendMessage(
+          this.config.telegram.chatId,
+          await retryMessage,
+          this.config.telegram.sendOptions,
+        );
+      }
+      updates = setTimeout(this.getUpdates, waitTime);
+      attempts++;
+    }
+
     // Determine the time window for allowed starts for a game
     const startDate = this.timegen.dateFromTime(this.config.times.staticWeekday[0]);
     // endDate = startDate + allowed window size. Window is in minutes, getTime() returns milliseconds
@@ -42,7 +66,7 @@ export default class FirstMonkku {
 
     // Parse messages and get start message
     const playerData = await this.parser.getPlayers(
-      await updates,
+      updates,
       this.config.monkku.commandPrefix,
       await startDate,
       endDate,
